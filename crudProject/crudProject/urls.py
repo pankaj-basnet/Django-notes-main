83d66fc25060f7dc51041d154e5d80ee7b63279c
Pankaj Basnet <pankajbasnet2020@hotmail.com>
2024-06-20 15:38:28 +0545

    added home.html in StudyBud_traversy_app

--- crudProject/crudProject/urls.py
+++ crudProject/crudProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from crudApp import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,5 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home)
+    path("", views.home),
+    path('studybud/', include('StudyBud_traversy_app.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
